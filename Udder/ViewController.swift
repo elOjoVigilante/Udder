015b1db1cffe9617f01aa355b27a41c3a131428f
Jose Romero <elojovigilante@gmail.com>
2019-07-10 17:35:17 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jose Andres Romero on 7/10/19.
+//  Copyright © 2019 Jose Andres Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
